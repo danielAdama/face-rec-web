c82d10318194dec5157a7f20b5d6843bc5798704
danielAdama <adamadaniel321@gmail.com>
2023-03-19 23:10:07 +0100

    fix

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ import os
 import cv2
 
 MODEL = 'hog'
-DATAPATH = r'C:\\Users\\DELL\\Desktop\\programming\\computerVision\\face_recognition\\'
+DATAPATH = r'/home/chuby/Desktop/programming/computerVision/face_recognition/face-rec-web'
 KNOWN_PATH = os.path.join(DATAPATH,'known_face')
 INPUT_PATH = os.path.join(DATAPATH, 'input_face')
 ##VIDEO_PATH = r'/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/video_for_vision_experiment'
